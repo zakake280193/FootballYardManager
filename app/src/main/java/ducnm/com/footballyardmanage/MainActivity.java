1d35cd0c87d158dd51528015f94be76834bfc8dc
gcstester01 <gcstester01@gmail.com>
2017-07-07 10:26:30 +0700

    Add layout for login and signIn  page

--- app/src/main/java/ducnm/com/footballyardmanage/MainActivity.java
+++ app/src/main/java/ducnm/com/footballyardmanage/MainActivity.java
@@ -1,14 +1,36 @@
 package ducnm.com.footballyardmanage;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    EditText txtEmail, txtPassword;
+    Button btnRegister, btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("Hello git");
+
+        txtEmail = (EditText) findViewById(R.id.txtEmail);
+        txtPassword = (EditText) findViewById(R.id.txtPassword);
+        btnRegister = (Button) findViewById(R.id.btnRegister);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/ducnm/com/footballyardmanage/RegisterActivity.java
@@ -0,0 +1,14 @@
+package ducnm.com.footballyardmanage;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
